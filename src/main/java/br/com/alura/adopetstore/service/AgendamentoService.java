3aaf7d5d6bb43a10ca36ba4609a95cef785db6c5
Gabriel Soares de Souza <gsoaresdesouza@live.com>
2026-03-23 20:35:59 -0300

    Agendando uma tarefa com Spring

---
+++ src/main/java/br/com/alura/adopetstore/service/AgendamentoService.java
@@ -0,0 +1,21 @@
+package br.com.alura.adopetstore.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgendamentoService {
+
+    @Autowired
+    private RelatorioService relatorioService;
+
+    @Scheduled(cron = "0 35 20 * * *")
+    public void envioEmailsAgendado() {
+        var estoqueZerado = relatorioService.infoEstoque();
+        var faturamentoObtido = relatorioService.faturamentoObtido();
+
+        System.out.println(estoqueZerado);
+        System.out.println(faturamentoObtido);
+    }
+}
