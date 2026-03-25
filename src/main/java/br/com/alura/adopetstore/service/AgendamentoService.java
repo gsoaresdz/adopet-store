cc1542bb31f401a9a028870a22fe74644ac6f1d9
Gabriel Soares de Souza <gsoaresdesouza@live.com>
2026-03-24 21:04:31 -0300

    Evoluindo o envio de relatórios da aplicação

--- src/main/java/br/com/alura/adopetstore/service/AgendamentoService.java
+++ src/main/java/br/com/alura/adopetstore/service/AgendamentoService.java
@@ -1,5 +1,6 @@
 package br.com.alura.adopetstore.service;
 
+import br.com.alura.adopetstore.email.EmailRelatorioGerado;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -10,12 +11,15 @@ public class AgendamentoService {
     @Autowired
     private RelatorioService relatorioService;
 
-    @Scheduled(cron = "0 35 20 * * *")
+    @Autowired
+    private EmailRelatorioGerado enviador;
+
+    @Scheduled(cron = "0 04 21 * * *")
     public void envioEmailsAgendado() {
         var estoqueZerado = relatorioService.infoEstoque();
         var faturamentoObtido = relatorioService.faturamentoObtido();
 
-        System.out.println(estoqueZerado);
-        System.out.println(faturamentoObtido);
+        enviador.enviar(estoqueZerado, faturamentoObtido);
+        System.out.println("Thread do Agendamento: " + Thread.currentThread().getName());
     }
 }
